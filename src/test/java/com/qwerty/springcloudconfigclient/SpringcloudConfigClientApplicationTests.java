d14d885584e4d6b22df6a5da679c7f1c24b20604
zhou <2891128968@qq.com>
2019-06-08 04:09:05 +0800

    commit

--- src/test/java/com/qwerty/springcloudconfigclient/SpringcloudConfigClientApplicationTests.java
+++ src/test/java/com/qwerty/springcloudconfigclient/SpringcloudConfigClientApplicationTests.java
@@ -2,15 +2,27 @@ package com.qwerty.springcloudconfigclient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringcloudConfigClientApplicationTests {
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
 	@Test
 	public void contextLoads() {
+		System.out.println("-------------------------------------------------------");
+		System.out.println(webApplicationContext);
+		System.out.println("-------------------------------------------------------");
+		System.out.println(webApplicationContext.getBean("dataSource"));
+		System.out.println("-------------------------------------------------------");
+		System.out.println(webApplicationContext.getBean("oracleDataSource"));
+		System.out.println("-------------------------------------------------------");
 	}
 
 }
