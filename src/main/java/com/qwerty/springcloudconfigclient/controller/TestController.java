865cf20257f2b6ce69acf1a189c76f525d1918a3
zhou <2891128968@qq.com>
2019-06-08 15:38:55 +0800

    2019.6.8 14:32 注册中心注册配置客户端

--- src/main/java/com/qwerty/springcloudconfigclient/controller/TestController.java
+++ src/main/java/com/qwerty/springcloudconfigclient/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.qwerty.springcloudconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
+@RefreshScope
 public class TestController {
 
     @Value("${test}")
